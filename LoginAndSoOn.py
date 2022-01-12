0f447b1a416048b11b0ed7cf1dc663d31c4b5f53
opilane <opilane@HP-CZC6297B8Z>
2022-01-12 10:55:12 +0200

    stuff

--- LoginAndSoOn.py
+++ LoginAndSoOn.py
@@ -1,20 +1,15 @@
 from module1 import *
-user1="bigDaddySvin"
-user2="GreatGuy"
-user3="bunda"
-users=[user1, user2, user3]
-password1=("mamasvin")
-password2=("GreatestGuy")
-password3=("ULTRAbunda228")
+["bigDaddySvin", "GreatGuy", "bunda"]
+["mamasvin","GreatestGuy","ULTRAbunda228"]
 print("Hello!")
-print("To access the application you need to register")
-answer=int(input("Do you want to? 1 for Yes, 2 for No: "))
+print("To access the application you need to log in")
+answer=int(input("Do you have an account? 1 for Yes, 2 for No: "))
 while 1:
 	if answer==1:
-		print("Great!")
+		print("Great! Lets log in")
+		logIn()
+		print("Congrats! You've logged in!")
 	elif answer==2:
-		print("Sad...")
-		print("You may exit the programm, by typping 'exit' or you can register by typping 'reg'")
-		a=input("")
-		if a=="exit":
-			break
+		print("Allright, Lets create one!")
+		newAc()
+		print("Congrats! You've created yourself a new account")
--- module1.py
+++ module1.py
@@ -31,10 +31,58 @@ def newAc():
 		print(f"Your generated password: {password}")
 	elif choice=="create my own":
 		password=input("Enter your new password: ")
-		print(f"You own password: {password}") #rabotaet no ne dokontsa
+		a=passwordcheck(password)
+		while a==False:
+			print("Your password must contain numbers, lower cases and upper cases!")
+			password=input("Enter your new password: ")
+			a=passwordcheck(password)
+			if a==True:
+				break
+	print(f"Your own password: {password}")
 
-def passwordcheck(password):
+def passwordcheck(password)->bool:
 	"""
 	"""
-	[]#spisk dlja znakov
-	if (password.isupper()) and (password.isdigit()) and (password.islower()) and password in []#znaki
+	cond=0
+	passwordChek=list(password)
+	for i in passwordChek:
+		if i.isupper() and i.isdigit() and i.islower() and i.islapha():
+			cond=True
+		else:
+			cond=False
+	return cond#dorabotat
+
+def logIn():
+	"""
+	"""
+	un=input("Enter your username: ")
+	print(f"Hello, {un}!")
+	pw=input("Enter your password: ")
+	a=logIncheck(un,pw)
+	while a==0:
+		print("Either username was incorrect or password. Try again")
+		un=input("Enter your username: ")
+		print(f"Hello, {un}!")
+		pw=input("Enter your password: ")
+		a=logIncheck(un,pw)
+		if a==True:
+			break
+
+def logIncheck(username,password)->bool:
+	"""
+	"""
+	condUn=0
+	condPw=0
+	users=["bigDaddySvin", "GreatGuy", "bunda"]
+	passwords=["mamasvin","GreatestGuy","ULTRAbunda228"]
+	if username not in users:
+		print("Incorrect username!")
+		condUn=False
+	else:
+		True
+	if password not in passwords:
+		print("Incorrect password!")
+		condPw=False
+	else:
+		True
+	return condUn and condPw#dorabotat
